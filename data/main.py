28fe3a22936bce62b444f77ad270f2303598970b
up202004999 <duarte.eduardoeds@gmail.com>
2023-10-03 09:59:15 +0100

    well its something I just dont know what

---
+++ data/main.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+
+# Load and preprocess data for each table
+awards_players_df = pd.read_csv('awards_players.csv')
+coaches_df = pd.read_csv('coaches.csv')
+players_df = pd.read_csv('players.csv')
+players_teams_df = pd.read_csv('players_teams.csv')
+series_post_df = pd.read_csv('series_post.csv')
+teams_df = pd.read_csv('teams.csv')
+teams_post_df = pd.read_csv('teams_post.csv')
+
+# Preprocess the 'teams' DataFrame to create a target variable 'playoff'
+teams['playoff'] = ...  # Create this column based on your criteria, "yes" or "no"
+
+# Select relevant features from the 'teams' DataFrame
+features = teams[['feature1', 'feature2', ...]]  # Replace with actual feature names
+
+# Select the target variable 'playoff' from the 'teams' DataFrame
+target = teams['playoff']
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
+
+# Choose a model (e.g., Random Forest)
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+
+# Train the model
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+predictions = model.predict(X_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, predictions)
+report = classification_report(y_test, predictions)
+
+# Print the results
+print(f"Accuracy: {accuracy}")
+print("Classification Report:")
+print(report)
+
+# Make predictions for the next season
+# Assuming you have a DataFrame 'next_season_teams' containing data for the next season
+# Preprocess this data similarly to how you preprocessed the training data
+
+next_season_predictions = model.predict(next_season_teams)
+
+# Identify teams that qualify for playoffs in the next season
+next_season_teams['predicted_playoff'] = next_season_predictions
+playoff_teams_next_season = next_season_teams[next_season_teams['predicted_playoff'] == 'yes']
